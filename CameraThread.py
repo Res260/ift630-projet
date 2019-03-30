194ad566f85795136ab278c0216d15bbb3d66470
Émilio Gonzalez <egg997@gmail.com>
2019-03-30 18:18:34 -0400

    Captures N seconds of camera

--- CameraThread.py
+++ CameraThread.py
@@ -1,6 +1,7 @@
 import logging
 import threading
 import time
+from collections import deque
 from queue import Queue
 
 import cv2
@@ -11,10 +12,11 @@ class CameraThread(threading.Thread):
     Thread that fetches camera's content.
     """
 
-    def __init__(self):
+    def __init__(self, capture_duration: int):
         super().__init__()
+        self.capture_duration = capture_duration
         self.logger = logging.getLogger('main.camera')
-        self.queue = Queue()
+        self.queue = deque()
         self.continue_running = True
 
     def run(self):
@@ -25,10 +27,23 @@ class CameraThread(threading.Thread):
         while self.continue_running:
             pass
             _, frame = cap.read()
-            self.queue.put(frame)
+            current_time = time.time()
+            self.remove_old_frames(current_time)
+            self.queue.append((current_time, frame))
             # cv2.imshow('frame',frame)
             # if cv2.waitKey(1) & 0xFF == ord('q'):
             #     break
+            self.logger.info(len(self.queue))
         cap.release()
         self.logger.debug("Camera thread stopped")
+
+    def remove_old_frames(self, current_time):
+        if len(self.queue) > 0:
+            continue_popping = True
+            while continue_popping:
+                if current_time - self.queue[0][0] > self.capture_duration:
+                    # remove last frames
+                    self.queue.popleft()
+                else:
+                    continue_popping = False
 
--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import argparse
 import signal
 import logging
 import os
@@ -7,10 +8,11 @@ from CameraThread import CameraThread
 
 
 class App:
-    def __init__(self):
+    def __init__(self, args: argparse.Namespace):
+        self.args = args
         self.logger = logging.getLogger('main')
         self.prepare_logger()
-        self.camera_thread = CameraThread()
+        self.camera_thread = CameraThread(args.capture_duration)
 
     def prepare_logger(self):
         self.logger.setLevel(logging.DEBUG)
@@ -20,7 +22,6 @@ class App:
 
     def run(self):
         self.camera_thread.start()
-        self.logger.info("ahah")
         while self.camera_thread.continue_running:
             # This is needed since thread.join() blocks signals...
             time.sleep(1)
@@ -32,6 +33,9 @@ class App:
 
 
 if __name__ == "__main__":
-    app = App()
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--capture-duration', '-d', help='Max capture duration (in seconds)', default='300', type=int)
+    args = parser.parse_args()
+    app = App(args)
     signal.signal(signal.SIGINT, app.trigger)
     app.run()
